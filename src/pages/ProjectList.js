0b7a2bb5cb85d6577c12f8453da74e2594cdb431
enahoroenas <orieroenahoro@yahoo.com>
2022-12-18 02:03:31 -0800

    added pw app

--- src/pages/ProjectList.js
+++ src/pages/ProjectList.js
@@ -5,6 +5,10 @@ const ProjectList = ({ name, des, projectlink, techused }) => {
 
   //const colors = ['#43A6C6','#8181F7',"#001CCE",'#BF40BF',"#4B088A","#FF8042","#1F618D",'#FAD02C','#001f47','#000230',"#FF1042"]
 
+  const openInNewTab = url => {
+    window.open(url, '_blank', 'noopener,noreferrer');
+  };
+
   return (
     <Wrapper>
     
@@ -30,8 +34,8 @@ const ProjectList = ({ name, des, projectlink, techused }) => {
         </div>
         
       <div className="cover">
-        <div className="demo">
-          <a className='link' href={projectlink}> Visit Website </a>
+        <div className="demo" onClick={() => openInNewTab(projectlink)}>
+          <p className='link'><u>Visit Website</u></p>
           </div>
       </div>        
             
